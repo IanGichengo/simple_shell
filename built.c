eb79e33f48a1dafef1de0d3e216524febe7d5d5b
IanGichengo <iangatimu6@gmail.com>
2024-02-04 07:49:30 -0500

    simple_shell

--- built.c
+++ built.c
@@ -1,17 +1,16 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: exits with a given exit status
- *         (0) if info.argv[0] != "exit"
+ * _myexit - exits shell
+ * @info: Structure containing potential arguments.
+ * Return: exits and gives exit status
  */
+
 int _myexit(info_t *info)
 {
 	int exitcheck;
 
-	if (info->argv[1])  /* If there is an exit argument */
+	if (info->argv[1])
 	{
 		exitcheck = _erratoi(info->argv[1]);
 		if (exitcheck == -1)
@@ -28,12 +27,13 @@ int _myexit(info_t *info)
 	info->err_num = -1;
 	return (-2);
 }
+
 /**
- * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * _mycd - changes from one directory to another
+ * @info: potential arguments
+ * Return: Always 0
  */
+
 int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
@@ -77,19 +77,16 @@ int _mycd(info_t *info)
 	}
 	return (0);
 }
+
 /**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * _myhelp - shows the man
+ * @info: potential argument
+ * Return: Always 0
  */
+
 int _myhelp(info_t *info)
 {
-	char **arg_array;
-
-	arg_array = info->argv;
+	(void)info;  /* Avoid unused parameter warning */
 	_puts("help call works. Function not yet implemented \n");
-	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
 	return (0);
 }
